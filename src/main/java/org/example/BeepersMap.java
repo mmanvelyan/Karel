242865f2f28558ecb631bd137ed65894fd998ed3
mmanvelyan <mmanvelyan2004q@gmail.com>
2024-03-14 11:31:27 +0200

    beepers map class

---
+++ src/main/java/org/example/BeepersMap.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BeepersMap {
+    private final Map<Coordinates, Integer> beepers;
+
+    public BeepersMap(Map<Coordinates, Integer> beepers) {
+        this.beepers = beepers;
+    }
+
+    public BeepersMap(){
+        this(new HashMap<>());
+    }
+
+    public int getBeepersCount(int x, int y){
+        if (beepers.get(new Coordinates(x, y)) == null){
+            return 0;
+        } else {
+            return beepers.get(new Coordinates(x, y));
+        }
+    }
+
+    public Map<Coordinates, Integer> getBeepers() {
+        return new HashMap<>(beepers);
+    }
+}
