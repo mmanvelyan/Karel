2331364331e857b214f6d629e8f9a9347fe15487
mmanvelyan <mmanvelyan2004q@gmail.com>
2024-03-14 11:31:15 +0200

    added bag

--- src/main/java/org/example/RobotPosition.java
+++ src/main/java/org/example/RobotPosition.java
@@ -2,13 +2,15 @@ package org.example;
 
 public class RobotPosition {
     Coordinates position;
+    private final int bag;
 
-    public RobotPosition(Coordinates position){
+    public RobotPosition(Coordinates position, int bag){
         this.position = position;
+        this.bag = bag;
     }
 
-    public RobotPosition(int x, int y, Direction direction){
-        this(new Coordinates(x, y, direction));
+    public RobotPosition(int x, int y, Direction direction, int bag){
+        this(new Coordinates(x, y, direction), bag);
     }
 
     public int getX(){
@@ -21,5 +23,9 @@ public class RobotPosition {
 
     public Direction getDirection(){
         return position.getDirection();
+    }
+
+    public int getBag() {
+        return bag;
     }
 }
